9670841f4c551c75d4ed7e8d3b628dee03bf3a32
natiht <nht249@nyu.edu>
2021-06-30 16:00:36 +0400

    Revert "removed"
    This reverts commit c3cbeb44277a2de2aaf4562cc7fcf5a4bdecc5e9.

--- Main/Routes/routes.py
+++ Main/Routes/routes.py
@@ -1,12 +1,8 @@
-from Main.api.coopapi import Coopapi, Coopapi_coopid, Coopapi_leaderid, Coopapi_memberid, Coopsapi
 from Main.api.userapi import UserApi, UsersApi
 from Main.api.authapi import SignupApi, LoginApi
 
 def initialize_routes(api):
-    api.add_resource(UsersApi, '/api/users')
-    api.add_resource(UserApi, '/api/user/<uid>')
-    api.add_resource(SignupApi, '/api/auth/signup')
-    api.add_resource(LoginApi, '/api/auth/login')
-    api.add_resource(Coopapi,'/api/coop')
-    api.add_resource(Coopapi_coopid,'/api/coop/<Coopid>')
-    api.add_resource(Coopapi_memberid,'/api/coop/member/<Member_id>/<Coop_id>')
+ api.add_resource(UsersApi, '/api/users')
+ api.add_resource(UserApi, '/api/user/<uid>')
+ api.add_resource(SignupApi, '/api/auth/signup')
+ api.add_resource(LoginApi, '/api/auth/login')
