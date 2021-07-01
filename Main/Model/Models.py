43be6e52320ed17cee6021c82e09e910d2c98007
NatiTsegaye <86118205+NatiTsegaye@users.noreply.github.com>
2021-07-01 10:08:39 +0400

    Add files via upload

---
+++ Main/Model/Models.py
@@ -0,0 +1,30 @@
+from  Main.DAC.config import db
+from flask_bcrypt import generate_password_hash, check_password_hash
+#from Main.Model.Coop import Coop
+
+class Coop(db.Document):    
+    Coopid = db.StringField(required=True, unique=True)
+    #image = db.ImageFeild()
+    Name = db.StringField(required=True, unique=True)
+    Leader = db.ReferenceField('User')
+    Members = db.ListField(db.ReferenceField('User'))
+    #Products = db.ListField(db.ReferenceField('Products', reverse_delete_rule=db.PULL))
+
+class User(db.DynamicDocument):
+    UID = db.StringField(required=True, unique=True)
+    #image = db.ImageFeild()
+
+    mobile = db.StringField(required=True, unique=True)
+    username = db.StringField(required=True, unique=True)
+    category = db.ListField(db.StringField(), required=True)
+    _password = db.StringField(required=True)
+    coopL = db.ListField(db.ReferenceField('Coop', reverse_delete_rule=db.PULL))
+    #coopmember = db.ListField(db.ReferenceField('Coop', reverse_delete_rule=db.PULL))
+    def hash_password(self):
+        self._password = generate_password_hash(self._password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
+
+
+Coop.register_delete_rule(User,"Members",db.PULL)
